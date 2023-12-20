62e52334479fdc7962bf35e8d87d8546f16111b8
Vijay0806 <vijaym08062001@gmail.com>
2023-12-20 16:24:35 +0530

    changes

--- src/Project.js
+++ src/Project.js
@@ -30,11 +30,11 @@ function Project() {
                                 <h4 className="project__text">Airbnb Clone</h4>
                                 <h6 className="project__text">Description</h6>
                                 <p className="project__text">A replica of the Airbnb platform, allowing users to easily create accounts, discover accommodations through search, and list their properties for rent, offering a seamless experience for both hosts and guests.</p>
-                                <div className="demo_details">
+                                {/* <div className="demo_details">
                                     <h6 className="demo_details">Demo Credential</h6>
                                     <p className="demo_details">username :</p>
                                     <p className="demo_details">password :</p>
-                                </div>
+                                </div> */}
                                 {/* <h5 className="project__text">Capstone Project</h5> */}
                                 {/* <h4 className="project__text">Airbnb Clone</h4> */}
                                 {/* <h6>Crafted a dynamic Airbnb clone, seamlessly connecting travelers with unique accommodations through a user-friendly platform.</h6> */}
@@ -59,11 +59,11 @@ function Project() {
                                 <h4 className="project__text">Stack overflow Clone</h4>
                                 <h6 className="project__text">Description</h6>
                                 <p className="project__text">This is an clone of StackOverFlow website. Where we can create account,see solution by search, post our question in public for which we need solution and we can post our solution to the question.</p>
-                                <div className="demo_details">
+                                {/* <div className="demo_details">
                                     <h6 className="demo_details">Demo Credential</h6>
                                     <p className="demo_details">username :</p>
                                     <p className="demo_details">password :</p>
-                                </div>
+                                </div> */}
                                 <div>
                                     <a href="https://github.com/Vijay0806/Webcode-Frontend-2" className="project__btn" target={"_blank"}>Frontend Link</a>
                                     <a href="https://github.com/Vijay0806/Webcode-Backend-2" className="project__btn" target={"_blank"}>Backend Link</a>
@@ -85,11 +85,11 @@ function Project() {
                                 <h4 className="project__text">Movie App</h4>
                                 <h6 className="project__text">Description</h6>
                                 <p className="project__text">Discover and watch your favorite movies effortlessly with our user-friendly app.Where we can create account,Enjoy endless entertainment tailored just for you!</p>
-                                <div className="demo_details">
+                                {/* <div className="demo_details">
                                     <h6 className="demo_details">Demo Credential</h6>
                                     <p className="demo_details">username :</p>
                                     <p className="demo_details">password :</p>
-                                </div>
+                                </div> */}
                                 <div>
                                     <a href="https://github.com/Vijay0806/Movie-Frontend" className="project__btn" target={"_blank"}>Frontend Link</a>
                                     <a href="https://github.com/Vijay0806/Movie_Backend" className="project__btn" target={"_blank"}>Backend Link</a>
@@ -111,11 +111,11 @@ function Project() {
                                 <h4 className="project__text">Library Management</h4>
                                 <h6 className="project__text">Description</h6>
                                 <p className="project__text">Where we can create account,Facilitate easy book tracking and borrower management with Library Management, ensuring a well-organized and accessible library experience for users.</p>
-                                <div className="demo_details">
+                                {/* <div className="demo_details">
                                     <h6 className="demo_details">Demo Credential</h6>
                                     <p className="demo_details">username :</p>
                                     <p className="demo_details">password :</p>
-                                </div>
+                                </div> */}
                                 <div>
                                     <a href="https://github.com/Vijay0806/React-frontend-7" className="project__btn" target={"_blank"}>Frontend Link</a>
                                     <a href="https://github.com/Vijay0806/React--Backend-1" className="project__btn" target={"_blank"}>Backend Link</a>
@@ -137,11 +137,11 @@ function Project() {
                                 <h4 className="project__text">Reset PassWord with auth</h4>
                                 <h6 className="project__text">Description</h6>
                                 <p className="project__text">This Application allows the user to register with mail verification, login, reset password with mail verification</p>
-                                <div className="demo_details">
+                                {/* <div className="demo_details">
                                     <h6 className="demo_details">Demo Credential</h6>
                                     <p className="demo_details">username :</p>
                                     <p className="demo_details">password :</p>
-                                </div>
+                                </div> */}
                                 <div className="button_box">
                                     <a href="https://github.com/Vijay0806/Node-Task-5-Frontend" className="project__btn" target={"_blank"}>Frontend Link</a>
                                     <a href="https://github.com/Vijay0806/Node-Task-5-Backend" className="project__btn" target={"_blank"}>Backend Link</a>
